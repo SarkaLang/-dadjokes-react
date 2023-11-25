9302565b025962409bbe1719e3e4d0a9035a2a67
SarkaLang <sarka.langy@gmail.com>
2023-11-25 10:17:24 +0100

    zalozeni-projektu

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <header>
+        <div className="logo" />
+        <h1>Dad jokes - react</h1>
+      </header>
+      <main>
+      </main>
+      <footer>
+        <p>Czechitas, Digitální akademie: Web - Test</p>
+      </footer>
+    </div>
+  );
+};
